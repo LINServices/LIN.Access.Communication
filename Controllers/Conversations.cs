55096adf9a9b72bcde027916234c7000174507e8
Alex <giraldojhong4@gmail.com>
2023-09-03 14:03:35 -0500

    Obtener miembros de una conversacion

--- Controllers/Conversations.cs
+++ Controllers/Conversations.cs
@@ -49,4 +49,49 @@ public static class Conversations
     }
 
 
+
+    /// <summary>
+    /// Obtiene los mensajes asociados a una conversación
+    /// </summary>
+    /// <param name="idConversation">ID de la conversación</param>
+    public async static Task<ReadAllResponse<MemberChatModel>> Members(int idConversation)
+    {
+
+        // Crear HttpClient
+        using var httpClient = new HttpClient();
+
+        httpClient.DefaultRequestHeaders.Add("token", "");
+
+        // ApiServer de la solicitud GET
+        string url = ApiServer.PathURL($"conversations/{idConversation}/members");
+
+        try
+        {
+
+            // Hacer la solicitud GET
+            var response = await httpClient.GetAsync(url);
+
+            // Leer la respuesta como una cadena
+            string responseBody = await response.Content.ReadAsStringAsync();
+
+            var obj = JsonConvert.DeserializeObject<ReadAllResponse<MessageModel>>(responseBody);
+
+            return obj ?? new();
+
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Error al hacer la solicitud GET: {e.Message}");
+        }
+
+
+        return new();
+
+
+
+
+
+    }
+
+
 }
