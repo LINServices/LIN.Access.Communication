2162950f18fd1907d41839f8a89d5d73877d0ff7
Alex <giraldojhong4@gmail.com>
2023-10-03 12:21:21 -0500

    Create Access

--- Controllers/Conversations.cs
+++ Controllers/Conversations.cs
@@ -1,4 +1,5 @@
 ﻿using LIN.Types.Auth.Abstracts;
+using System.Reflection;
 
 namespace LIN.Access.Communication.Controllers;
 
@@ -32,6 +33,53 @@ public static class Conversations
             string responseBody = await response.Content.ReadAsStringAsync();
 
             var obj = JsonConvert.DeserializeObject<ReadAllResponse<MemberChatModel>>(responseBody);
+
+            return obj ?? new();
+
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Error al hacer la solicitud GET: {e.Message}");
+        }
+
+
+        return new();
+
+
+
+
+
+    }
+
+
+
+    /// <summary>
+    /// Obtiene las conversaciones asociadas a un perfil
+    /// </summary>
+    /// <param name="token">Token de acceso</param>
+    public async static Task<CreateResponse> Create(ConversationModel modelo)
+    {
+
+        // Crear HttpClient
+        using var httpClient = new HttpClient();
+
+
+        // ApiServer de la solicitud GET
+        string url = ApiServer.PathURL("conversations/create");
+        var json = JsonConvert.SerializeObject(modelo);
+
+        try
+        {
+            // Contenido
+            StringContent content = new(json, Encoding.UTF8, "application/json");
+
+            // Envía la solicitud
+            var response = await httpClient.PostAsync(url, content);
+
+            // Lee la respuesta del servidor
+            var responseContent = await response.Content.ReadAsStringAsync();
+
+            var obj = JsonConvert.DeserializeObject<CreateResponse>(responseContent);
 
             return obj ?? new();
 
@@ -170,6 +218,52 @@ public static class Conversations
             string responseBody = await response.Content.ReadAsStringAsync();
 
             var obj = JsonConvert.DeserializeObject<ReadAllResponse<SessionModel<MemberChatModel>>>(responseBody);
+
+            return obj ?? new();
+
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Error al hacer la solicitud GET: {e.Message}");
+        }
+
+
+        return new();
+
+
+
+
+
+    }
+
+
+
+    public async static Task<ReadAllResponse<SessionModel<ProfileModel>>> SearchProfiles(string pattern, string token)
+    {
+
+        // Crear HttpClient
+        using var httpClient = new HttpClient();
+
+        httpClient.DefaultRequestHeaders.Add("token", $"{token}");
+
+        // ApiServer de la solicitud GET
+        string url = ApiServer.PathURL($"profile/search");
+
+        url = Web.AddParameters(url, new()
+        {
+            {"pattern", pattern }
+        });
+
+        try
+        {
+
+            // Hacer la solicitud GET
+            var response = await httpClient.GetAsync(url);
+
+            // Leer la respuesta como una cadena
+            string responseBody = await response.Content.ReadAsStringAsync();
+
+            var obj = JsonConvert.DeserializeObject<ReadAllResponse<SessionModel<ProfileModel>>>(responseBody);
 
             return obj ?? new();
 
