fb5c1c90b6c55f98b762493b87809070429317be
Alex <giraldojhong4@gmail.com>
2023-10-21 17:09:07 -0500

    Messages controller update

--- Controllers/Messages.cs
+++ Controllers/Messages.cs
@@ -1,4 +1,6 @@
-﻿namespace LIN.Access.Communication.Controllers;
+﻿using LIN.Types.Emma.Models;
+
+namespace LIN.Access.Communication.Controllers;
 
 
 public static class Messages
@@ -48,6 +50,56 @@ public static class Messages
 
 
     }
+
+
+
+
+    /// <summary>
+    /// Obtiene las conversaciones asociadas a un perfil
+    /// </summary>
+    /// <param name="token">Token de acceso</param>
+    public async static Task<ReadOneResponse<ResponseIAModel>> ToEmma(string modelo, string token)
+    {
+
+        // Crear HttpClient
+        using var httpClient = new HttpClient();
+        httpClient.DefaultRequestHeaders.Add("token", token);
+
+
+        // ApiServer de la solicitud GET
+        string url = ApiServer.PathURL("emma");
+        var json = JsonConvert.SerializeObject(modelo);
+
+        try
+        {
+            // Contenido
+            StringContent content = new(json, Encoding.UTF8, "application/json");
+
+            // Envía la solicitud
+            var response = await httpClient.PostAsync(url, content);
+
+            // Lee la respuesta del servidor
+            var responseContent = await response.Content.ReadAsStringAsync();
+
+            var obj = JsonConvert.DeserializeObject<ReadOneResponse<ResponseIAModel>>(responseContent);
+
+            return obj ?? new();
+
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Error al hacer la solicitud GET: {e.Message}");
+        }
+
+
+        return new();
+
+
+
+
+
+    }
+
 
 
 }
