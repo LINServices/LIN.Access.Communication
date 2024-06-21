f22786d42bb67a96d3a1152bd548ca5d6274189c
Alex <giraldojhong4@gmail.com>
2024-06-21 06:24:54 -0500

    Update

--- Build.cs
+++ Build.cs
@@ -8,7 +8,7 @@ public class Build
     public static void Init()
     {
         Service._Service = new();
-        Service._Service.SetDefault("http://api.communication.linapps.co/");
+        Service._Service.SetDefault("https://api.communication.linplatform.com/");
         //Service._Service.SetDefault("http://localhost:5270/");
     }
 
